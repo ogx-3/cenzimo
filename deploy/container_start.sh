a4359784322de9fffc08dbb31f2736a6f0f5aa03
beiwei <3ogx.com@gmail.com>
2016-02-20 23:18:18 +0800

    Modify run shell

--- deploy/container_start.sh
+++ deploy/container_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /var/app/cenzimo && python manage.py migrate --noinput && python manage.py collectstatic --noinput
+cd /var/app/cenzimo && python manage.py migrate --noinput && python manage.py collectstatic 
 supervisord -n -c /etc/supervisor/supervisord.conf
