1fa0106a8220d42f80789853b9ea144191ebb429
beiwei <3ogx.com@gmail.com>
2016-02-21 21:59:40 +0800

    Add static file url

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -10,5 +12,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
